9729baa20c30bd9dc85c42c30f2501252d3d4b0e
Travis Swicegood <development@domain51.com>
2012-02-14 16:21:26 -0600

    Add attribution and anonymous fields

--- armstrong/apps/donations/models.py
+++ armstrong/apps/donations/models.py
@@ -86,6 +86,8 @@ class Donation(models.Model):
     amount = models.DecimalField(max_digits=9, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     processed = models.BooleanField(default=False)
+    attribution = models.CharField(max_length=255, default="")
+    anonymous = models.BooleanField()
 
     def save(self, **kwargs):
         if self.donation_type and not self.amount:
